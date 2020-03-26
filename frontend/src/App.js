d7ec49963e2c86504b99d2891826a7526cba9903
Brusangues <bruno99sr@gmail.com>
2020-03-25 23:23:04 -0300

    Avanços no Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+
+import Logon from './pages/Logon';
+
+import Routes from './routes';
+
+import './global.css'
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes />
   );
 }
 
 export default App;
+
+/*
+function App() {
+  const [counter, setCounter] = useState(0);
+    //retorna array com [valor, funcaoDeAtualização]
+
+  function increment() {
+    setCounter(counter +1);
+  }
+
+  return (
+    <div>
+      <Header>Contador: {counter}</Header>
+      <button onClick={increment}>Incrementar</button>
+    </div>
+    
+  );
+}
+*/
+
+// JSX: Quando o HTML está escrito dentro do arquivo js, ele é chamado JSX (JS XML)
+// COMPONENTE: Abaixo, um componente no react
+  //Componente é criado quando representa um trecho que se repete muito
+  //ou quando é possível isolá-lo
+
+//PROPRIEDADES: no React são análogas aos atributos do HTML
+  //Porém, são atributos repassados para componentes ao invés de elementos do HTML
+
+//ESTADO: Informação que será mantida pelo componente
+  //IMUTABILIDADE: Dentro do react nunca podemos manipular variáveis diretamente (por performance)
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDOM from 'react-dom'; // Integraçaõ do react com o navegador
+import App from './App'; // Importando o app
 
+//renderiza o app dentro da div com id root
+  //componente no react é uma função que retorna html
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -11,7 +11,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
